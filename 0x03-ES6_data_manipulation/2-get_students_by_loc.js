18897a4f218d0af54904c5494e1f69bbb4290db1
russiantech <chrisjsmez@gmail.com>
2024-05-15 04:02:37 +0000

    2-get_students_by_loc.js

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,7 +1,5 @@
-// 2-get_students_by_loc.js
 function getStudentsByLocation(students, city) {
-    return students.filter(student => student.location === city);
+  return students.filter(student => student.location === city);
 }
 
 export default getStudentsByLocation;
-
