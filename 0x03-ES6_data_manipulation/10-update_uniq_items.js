f6744bc34488429963ee7d96fcbcf83803093537
russiantech <chrisjsmez@gmail.com>
2024-05-15 07:35:32 +0000

    10-update_uniq_items.js

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,13 +1,12 @@
 function updateUniqueItems(map) {
-    if (!(map instanceof Map)) {
-        throw new Error('Cannot process');
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+  for (const [key, value] of map) {
+    if (value === 1) {
+      map.set(key, 100);
     }
-    map.forEach((value, key) => {
-        if (value === 1) {
-            map.set(key, 100);
-        }
-    });
+  }
 }
 
 export default updateUniqueItems;
-
