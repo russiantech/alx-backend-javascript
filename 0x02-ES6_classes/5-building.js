d862fcff65b4bb28e30aa8440c1a06f2ef46f282
russiantech <chrisjsmez@gmail.com>
2024-05-16 08:17:12 +0000

    5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,7 +1,13 @@
 // 5-building.js
-
+//
 export default class Building {
   constructor(sqft) {
+    if (this.constructor !== Building) {
+      const props = Object.getOwnPropertyNames(this.constructor.prototype);
+      if (!props.find((e) => e === 'evacuationWarningMessage')) {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
+    }
     this._sqft = sqft;
   }
 
@@ -9,7 +15,10 @@ export default class Building {
     return this._sqft;
   }
 
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
+  set sqft(sqft) {
+    if ((typeof sqft !== 'number') && (sqft instanceof Number)) {
+      throw new TypeError('Sqft must be a number');
+    }
+    this._sqft = sqft;
   }
 }
