65294145f0bd15db7e1df296dc36b656aa89ad52
russiantech <chrisjsmez@gmail.com>
2024-05-16 08:27:29 +0000

    10-car.js

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,17 +1,17 @@
 // 10-car.js
-
-const brandSymbol = Symbol('brand');
-const motorSymbol = Symbol('motor');
-const colorSymbol = Symbol('color');
-
+//
 export default class Car {
   constructor(brand, motor, color) {
-    this[brandSymbol] = brand;
-    this[motorSymbol] = motor;
-    this[colorSymbol] = color;
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  static get [Symbol.species]() {
+    return this.prototype.constructor;
   }
 
   cloneCar() {
-    return new Car(this[brandSymbol], this[motorSymbol], this[colorSymbol]);
+    return new (this.constructor[Symbol.species])();
   }
 }
