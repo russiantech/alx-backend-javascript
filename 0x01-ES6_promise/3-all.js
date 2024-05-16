a4ee246a32bfe0ea2a58b634d801b8f2e6fc8e3d
russiantech <chrisjsmez@gmail.com>
2024-05-16 04:36:14 +0000

    3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,12 +1,15 @@
 // 3-all.js
-import { uploadPhoto, createUser } from "./utils";
+
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
-    .then(([photoResponse, userResponse]) => {
-      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+    .then(([photo, user]) => {
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
     });
 }
+
+//export default handleProfileSignup;
