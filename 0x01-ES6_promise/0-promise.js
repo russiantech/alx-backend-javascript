87c0b89710b26bf0caa402630c39f0867709d74f
russiantech <chrisjsmez@gmail.com>
2024-05-16 04:30:17 +0000

    0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,7 +1,8 @@
 // 0-promise.js
+
 export default function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
-    // For now, we can just resolve the promise
-    resolve('Success');
+    resolve();
+    reject();
   });
 }
