46ff58e368cbd9e35336ff638597e0e568d52824
russiantech <chrisjsmez@gmail.com>
2024-06-23 12:11:00 +0000

    spy & payment api request

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,22 @@
+// utils.js
+const Utils = {
+  calculateNumber: function (type, a, b) {
+    const roundedA = Math.round(a);
+    const roundedB = Math.round(b);
+
+    if (type === 'SUM') {
+      return roundedA + roundedB;
+    } else if (type === 'SUBTRACT') {
+      return roundedA - roundedB;
+    } else if (type === 'DIVIDE') {
+      if (roundedB === 0) {
+        return 'Error';
+      }
+      return roundedA / roundedB;
+    } else {
+      throw new Error('Invalid type');
+    }
+  }
+};
+
+module.exports = Utils;
