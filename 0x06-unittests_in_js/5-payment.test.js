866597f2af4be99b886d4f0d72f4a1853cfcba11
russiantech <chrisjsmez@gmail.com>
2024-06-23 12:18:02 +0000

    sending payment request to api

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,30 @@
+// 5-payment.test.js
+const sinon = require('sinon');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./5-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  let consoleLogSpy;
+
+  beforeEach(() => {
+    consoleLogSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    consoleLogSpy.restore();
+  });
+
+  it('should log "The total is: 120" when called with 100 and 20', () => {
+    sendPaymentRequestToApi(100, 20);
+
+    expect(consoleLogSpy.calledOnceWithExactly('The total is: 120')).to.be.true;
+    expect(consoleLogSpy.calledOnce).to.be.true;
+  });
+
+  it('should log "The total is: 20" when called with 10 and 10', () => {
+    sendPaymentRequestToApi(10, 10);
+
+    expect(consoleLogSpy.calledOnceWithExactly('The total is: 20')).to.be.true;
+    expect(consoleLogSpy.calledOnce).to.be.true;
+  });
+});
